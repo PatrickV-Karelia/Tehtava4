4de5b4f1f25829227faa392af824c7ffcaf74b9a
Patrick Varkoi <patrick.varkoi@edu.karelia.fi>
2024-10-29 21:05:33 +0200

    first commit

---
+++ App/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AppLogger;
+
+
+Console.WriteLine("Kutsutaan AppLogger-komponentin palvelua:");
+
+Logger.Log("Tänään on 29.10.2024 ja kello on nyt 14:21. Ulkona paistaa aurinko.");
