ad9b33687734c1b337187875582bd689d9a4e52d
Patrick Varkoi <patrick.varkoi@edu.karelia.fi>
2024-10-29 23:04:39 +0200

    Staged Changes

--- AppLogger/obj/Debug/net8.0/AppLogger.AssemblyInfo.cs
+++ AppLogger/obj/Debug/net8.0/AppLogger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppLogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc3d707bacfe217eacedbe73f9fb1ff3ebfe2715")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppLogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppLogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
