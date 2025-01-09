eac1ce2f0e55db502a60ba0dbc43c7c9b189e20c
Patrick Varkoi <patrick.varkoi@edu.karelia.fi>
2025-01-09 18:11:42 +0200

    Jatkuvia commiteja

--- AppLogger/obj/Debug/net8.0/AppLogger.AssemblyInfo.cs
+++ AppLogger/obj/Debug/net8.0/AppLogger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppLogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2852ca0b962a33713845c7c1632b1c8799d1d486")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be43a751d87335aacf5d7b715064ec6b42052824")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppLogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppLogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
