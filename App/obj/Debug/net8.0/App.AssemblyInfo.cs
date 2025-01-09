be43a751d87335aacf5d7b715064ec6b42052824
Patrick Varkoi <patrick.varkoi@edu.karelia.fi>
2025-01-09 18:11:13 +0200

    asdasdas

--- App/obj/Debug/net8.0/App.AssemblyInfo.cs
+++ App/obj/Debug/net8.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd3921c7c9f2d8f067898ca5d58070193a2a08b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2852ca0b962a33713845c7c1632b1c8799d1d486")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
